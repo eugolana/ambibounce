8589efe6cc45f8352271e6bdd07b4cd52ee38b4f
Sam Sharp <eugolana2@gmail.com>
2017-12-20 00:50:49 +0000

    added colour change on sound

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ masterGain.value = 0.05;
 masterGain.connect(audioContext.destination);
 
 var bgrnd = new Path.Rectangle(new Rectangle(new Point(0,0), new Point(canvas.width, canvas.height)));
-bgrnd.fillColor = new Color(192,192,192, 1);
+bgrnd.fillColor = new Color(0.8,0.8,0.8);
 
 var Ball = function(pos, size, weight){
 	this.pos = pos;
@@ -27,8 +27,13 @@ var Ball = function(pos, size, weight){
 	this.shell = Path.Circle(this.pos, size * 1.33);
 	this.shell.opacity = 0.2;
 
-	this.ball.addChildren([this.inner, this.mid, this.outer, this.shell]);
+	this.soundBall = Path.Circle(this.pos, size/3);
+	this.soundBall.opacity = 0.0;
+
+	this.ball.addChildren([this.inner, this.mid, this.outer, this.shell, this.soundBall]);
 	this.ball.fillColor = 'black';
+
+	this.soundBall.fillColor = 'white';
 	this.bounced = 0;
 }
 
@@ -43,6 +48,7 @@ Ball.prototype.run = function(gravity, lines) {
 				this.traj = this.traj.rotate(-180 - c, new Point(0,0))
 				this.bounced = 2;
 				line.sound(this);
+				this.soundBall.opacity = Math.min(0.8, this.soundBall.opacity + 0.4);
 			} else {
 				//
 			}
@@ -54,6 +60,7 @@ Ball.prototype.run = function(gravity, lines) {
 	this.traj += new Point(0, gravity);
 	this.pos += this.traj;
 	this.ball.translate(this.traj);
+	this.soundBall.opacity = this.soundBall.opacity * 0.99;
 }
 
 
@@ -64,16 +71,20 @@ var Line = function(posA, posB, soundOut) {
 	this.angle = (posB - posA).angle;
 	this.line = new Group()
 
+	this.gain = audioContext.createGain();
+	this.gain.gain.value = 0.8;
+	this.gain.connect(this.soundOut)
+
 	this.innerLine = new Path(this.posA, this.posB);
-	this.innerLine.strokeWidth = 1;
+	this.innerLine.strokeWidth = 1.7;
 	this.innerLine.opacity = 0.8;
 	this.innerLine.strokeCap = 'round';
-	this.innerLine.strokeColor = new Color(0,0,0,0.7);
+	this.innerLine.strokeColor = new Color(0,0,0,0.5);
 
 	this.midLine = new Path(this.posA, this.posB)
-	this.midLine.strokeWidth = 5;
+	this.midLine.strokeWidth = 7;
 	this.midLine.strokeCap = 'round';
-	this.midLine.strokeColor = new Color(0,0,0,0.2);
+	this.midLine.strokeColor = new Color(0,0,0,0.4);
 
 	this.outerLine = new Path(this.posA, this.posB);
 	this.outerLine.strokeColor = new Color(0,0,0,0.2);
@@ -81,9 +92,30 @@ var Line = function(posA, posB, soundOut) {
 	this.outerLine.strokeWidth = 10;
 	// this.outerLine.opacity = 0.3;
 
-	this.line.addChildren([this.innerLine, this.midLine, this.outerLine])
+	this.soundLine = new Path(this.posA, this.posB);
+	this.soundLine.strokeWidth = 1.7;
+	this.soundLine.strokeCap = 'round';
+	this.soundLine.strokeColor = new Color(255,255,255, 0.0);
+
+	this.line.addChildren([this.innerLine, this.midLine, this.outerLine, this.soundLine])
 	this.pitch = 440 / (this.outerLine.length/400);
 	this.oscList = [];
+
+	this.outerLine.onMouseEnter = function(event){
+		this.strokeColor = new Color(128, 0, 0, 0.2);
+	}
+	this.outerLine.onMouseLeave = function(event){
+		this.strokeColor = new Color(0, 0, 0, 0.2);
+	}
+	this.outerLine.onDoubleClick = this.deleteLine();
+	this.soundLine.onDoubleClick = this.deleteLine();
+
+}
+
+Line.prototype.run = function(){
+	if (this.soundLine.strokeColor.alpha > 0.0) {
+		this.soundLine.strokeColor.setAlpha(this.soundLine.strokeColor.alpha  * 0.99)
+	}
 }
 
 Line.prototype.updateLine = function(posB) {
@@ -97,6 +129,9 @@ Line.prototype.updateLine = function(posB) {
 	this.midLine.removeSegment(1);
 	this.midLine.add(this.posB)
 
+	this.soundLine.removeSegment(1);
+	this.soundLine.add(this.posB)
+
 	this.angle = (this.posB - this.posA).angle;
 	this.pitch = 440 / (this.innerLine.length/400);
 }
@@ -108,11 +143,12 @@ Line.prototype.sound = function(ball) {
 	ge = createEnv(0.1, 0.6, 0.5, 1.0, volume, audioContext);
 	gain = ge[0]
 	end = ge[1]
-	gain.connect(this.soundOut)
+	gain.connect(this.gain)
 	osc.connect(gain);
 	osc.start();
 	this.end = end;
 	this.oscList.push([osc, end]);
+	this.soundLine.strokeColor.setAlpha(Math.min(1.0, this.soundLine.strokeColor.alpha + 0.4));
 }
 
 Line.prototype.purge = function(now){
@@ -124,10 +160,21 @@ Line.prototype.purge = function(now){
 		if (end <= now) {
 			osc.stop()
 			this.oscList.splice(i, 1)
-			console.log('osc purged')
 		} else {
 			i++
 		}
+	}
+}
+
+Line.prototype.remove = function(){
+	this.line.remove();
+	this.line = false;
+}
+
+Line.prototype.deleteLine = function(){
+		var line = this;
+		return function(event) {
+			line.remove();
 	}
 }
 
@@ -137,15 +184,21 @@ var tempLine;
 balls = [];
 lines = [];
 
-var helloText =  new PointText(new Point(canvas.width/2, 20))
+var helloText =  new PointText(new Point(20, 15))
 
-helloText.content = "double click to spawn a ball. Click and drag to draw a line."
+helloText.content = "Double click to spawn a ball. Click and drag to draw a line."
 
 
 function onFrame(){
 	var now = audioContext.currentTime;
-	for (var i = 0; i < lines.length; i++) {
+	for (var i = 0; i < lines.length;) {
 		lines[i].purge(now);
+		if (!lines[i].line) {
+			lines.splice(i, 1);
+		} else {
+			lines[i].run();
+			i++;
+		}
 	}
 	for (var i = 0; i < balls.length; i++) {
 		balls[i].run(0.05, lines);
@@ -180,7 +233,7 @@ bgrnd.onDoubleClick = function(event) {
 
 function createEnv(a,d,s,r,volume, context) {
 	var gain = context.createGain();
-	gain.gain.value = 0.001;
+	gain.gain.value = 0.0;
 	var now = context.currentTime;
 	a = now + a;
 	d = a + d;
@@ -190,3 +243,4 @@ function createEnv(a,d,s,r,volume, context) {
 	gain.gain.linearRampToValueAtTime(0.0, r);
 	return [gain, r];
 }
+
